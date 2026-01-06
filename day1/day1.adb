3c6e8a0f9f14b1dc583d42a518880af8312a2512
Oliver Hookins <ohookins@gmail.com>
2026-01-06 20:54:14 +1100

    Actually read the files.

--- day1/day1.adb
+++ day1/day1.adb
@@ -1,24 +1,28 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Containers; use Ada.Containers;
 
 package body Day1 is
    function Read_Input(Use_Test_Input : Boolean) return String_Vectors.Vector is
       F : File_Type;
       File_Name : String :=
          (if Use_Test_Input
-            then "test.txt"
-            else "input.txt");
+            then "day1/test.txt"
+            else "day1/input.txt");
       I : String_Vectors.Vector;
    begin
       Open(F, In_File, File_Name);
       while not End_Of_File(F) loop
-         I.Append(Get_Line(F));
+         I.Append(To_Bounded_String(Get_Line(F)));
       end loop;
       return I;
    end Read_Input; 
 
    procedure Run(Use_Test_Input : Boolean) is
+      Input : String_Vectors.Vector;
    begin
-      Put_Line ("Hello world from day 1!" & 
-               (if Use_Test_Input then " (using test input)" else ""));
+      Input := Read_Input(Use_Test_Input);
+      Put_Line ("There are "
+                  & Count_Type'Image (Input.Length)
+                  & " elements in the input");
    end Run;
 end Day1;
--- day1/day1.ads
+++ day1/day1.ads
@@ -1,5 +1,6 @@
+with Ada.Strings; use Ada.Strings;
+with Ada.Strings.Bounded;
 with Ada.Containers.Vectors;
-with Ada.Strings.Bounded; use Ada.Strings.Bounded; 
 
 package Day1 is
    procedure Run(Use_Test_Input : Boolean);
@@ -7,12 +8,17 @@ package Day1 is
    private
    Dial_Location: integer := 50;
 
-   subtype String5 is String(1 .. 5);
+   -- this seems a little insane but the (1 .. 5) mechanism didn't work
+   -- for me
+   package BStr is new Ada.Strings.Bounded.Generic_Bounded_Length (Max => 5);
+   use BStr;
 
+   -- along similar lines, we need to instantiate the generic vector
+   -- specifically for our bounded string type
    package String_Vectors is new
      Ada.Containers.Vectors
        (Index_Type   => Natural,
-        Element_Type => String5);
+        Element_Type => Bounded_String);
 
    function Read_Input(Use_Test_Input : Boolean) return String_Vectors.Vector;
 end Day1;
